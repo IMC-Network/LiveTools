f0711083d2e6851b69eee55120f7972e44599517
James-Livesey <jamesliveg@gmail.com>
2019-12-08 15:36:58 +0000

    Add template list

--- prog.js
+++ prog.js
@@ -1,4 +1,5 @@
 var useEpisodeKey = "";
+var useTemplateKey = "";
 
 function userReady() {
     firebase.database().ref("orgs/" + currentUser.orgName + "/programmes/" + getURLParameter("prog")).on("value", function(snapshot) {
@@ -7,6 +8,7 @@ function userReady() {
         } else {
             $(".progName").text(snapshot.val().name || "Untitled");
             $(".progLink").attr("href", "prog.html?prog=" + encodeURIComponent(getURLParameter("prog")));
+            $(".progTemplatesLink").attr("href", "progTemplates.html?prog=" + encodeURIComponent(getURLParameter("prog")));
             $(".progDescription").text(snapshot.val().description || "No description provided.");
 
             firebase.database().ref("orgs/" + currentUser.orgName + "/programmes/" + getURLParameter("prog") + "/episodes").orderByChild("firstTXDate").on("value", function(snapshot) {
@@ -28,6 +30,7 @@ function userReady() {
                                     <tr>
                                         <th>Slug</th>
                                         <th>First TX date</th>
+                                        <th>Template used</th>
                                         <th width="120">Options</th>
                                     </tr>
                                 </thead>
@@ -41,6 +44,7 @@ function userReady() {
                             <tr>
                                 <td data-col="slug" class="tableColumnNoWrap"></td>
                                 <td data-col="firstTXDate" class="tableColumnNoWrap"></td>
+                                <td data-col="templateUsed" class="tableColumnNoWrap"></td>
                                 <td data-col="options" class="tableColumnNoWrap">
                                     <button data-option="open">Open</button>
                                     <button data-option="delete">Delete</button>
@@ -58,6 +62,15 @@ function userReady() {
                             minute: "numeric"
                         }) || "Unknown");
 
+                        if (childSnapshot.val().templateUsedKey != null) {
+                            $(".progEpisodes table > tbody [data-col='templateUsed']:first").append("<a>")
+                                .attr("href", "progEpisode.html?prog=" + encodeURIComponent(getURLParameter("prog")) + "&template=" + encodeURIComponent(childSnapshot.val().templateUsedKey))
+                                .text(childSnapshot.val().templateUsedSlug || "Untitled")
+                            ;
+                        } else {
+                            $(".progEpisodes table > tbody [data-col='templateUsed']:first").text("(None)");
+                        }
+
                         (function(key) {
                             $(".progEpisodes table > tbody [data-col='options']:first [data-option='delete']").click(function() {
                                 deleteEpisode(key);
@@ -67,84 +80,73 @@ function userReady() {
                 }
             });
 
+            firebase.database().ref("orgs/" + currentUser.orgName + "/programmes/" + getURLParameter("prog") + "/templates").orderByChild("firstTXDate").on("value", function(snapshot) {
+                $(".progTemplates").html("");
+
+                if (snapshot.val() == null) {
+                    $(".progTemplates").html(`
+                        <p>
+                            It appears that you don't have any templates for
+                            this programme yet. Add templates by pressing the
+                            <strong>New template</strong> button.
+                        </p>
+                        <p>
+                            Episodes can be made out of templates so that you
+                            can easily inherit the flow and structure of the
+                            programme. Templates define the skeleton of
+                            episodes, and can be reused again and again.
+                        </p>
+                    `);
+                } else {
+                    $(`
+                        <div class="tableHolder">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Slug</th>
+                                        <th>First TX date</th>
+                                        <th width="120">Options</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>
+                            </table>
+                        </div>
+                    `).appendTo(".progTemplates");
+
+                    snapshot.forEach(function(childSnapshot) {
+                        $(".progTemplates table > tbody").prepend($(`
+                            <tr>
+                                <td data-col="slug" class="tableColumnNoWrap"></td>
+                                <td data-col="firstTXDate" class="tableColumnNoWrap"></td>
+                                <td data-col="options" class="tableColumnNoWrap">
+                                    <button data-option="open">Open</button>
+                                    <button data-option="delete">Delete</button>
+                                </td>
+                            </tr>
+                        `));
+
+                        $(".progTemplates table > tbody [data-col='slug']:first").text(childSnapshot.val().slug || "Untitled");
+                        $(".progTemplates table > tbody [data-col='firstTXDate']:first").text(new Date(childSnapshot.val().firstTXDate).toLocaleDateString(lang.lang, {
+                            weekday: "long",
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                            hour: "numeric",
+                            minute: "numeric"
+                        }) || "Unknown");
+
+                        (function(key) {
+                            $(".progTemplates table > tbody [data-col='options']:first [data-option='delete']").click(function() {
+                                deleteTemplate(key);
+                            });
+                        })(childSnapshot.key);
+                    });
+                }
+            });
+
             $(".pageLoader").hide();
             $(".loadingSection").show();
         }
-    });
-}
-
-function newEpisode() {
-    dialog("New episode", `
-        <div>
-            Create a new episode as part of the programme. Templates will be
-            copied over so that you can easily create the episode.
-        </div>
-        <div class="spacedTop">
-            <label>
-                <span>Slug</span>
-                <input placeholder="Enter the name of the episode" class="newEpisodeSlug">
-            </label>
-            <label>
-                <span>First TX date</span>
-                <input type="datetime-local" class="newEpisodeFirstTXDate">
-            </label>
-        </div>
-        <p class="dialogError"></p>
-    `, [
-        {text: "Cancel", onclick: "closeDialog();", type: "secondary"},
-        {text: "Create", onclick: "newEpisodeAction();", type: "primary"}
-    ]);
-}
-
-function newEpisodeAction() {
-    $(".dialog button:last").attr("disabled", "true");
-
-    if ($(".newEpisodeFirstTXDate").val().trim() == "" || $(".newEpisodeFirstTXDate").val().trim() == "" || isNaN(new Date($(".newEpisodeFirstTXDate").val().trim()).getTime())) {
-        $(".dialog button:last").attr("disabled", null);
-
-        $(".dialogError").text("It appears that you have left some fields blank or incorrectly formatted. Please enter data into those fields and try again.");
-    } else {
-        firebase.database().ref("orgs/" + currentUser.orgName + "/programmes/" + getURLParameter("prog") + "/episodes").push().set({
-            slug: $(".newEpisodeSlug").val().trim(),
-            firstTXDate: new Date($(".newEpisodeFirstTXDate").val().trim()).getTime()
-        }).then(function() {
-            $(".dialog button:last").attr("disabled", null);
-
-            closeDialog();
-        }).catch(function() {
-            $(".dialog button:last").attr("disabled", null);
-
-            $(".dialogError").text("Your new programme description could not be saved. Please try again later.");
-        });
-    }
-}
-
-function deleteEpisode(episodeKey) {
-    useEpisodeKey = episodeKey;
-
-    dialog("Delete episode", `
-        <div>
-            Do you really want to delete this episode? Once deleted, the
-            episode cannot be recovered.
-        </div>
-        <p class="dialogError"></p>
-    `, [
-        {text: "Cancel", onclick: "closeDialog();", type: "secondary"},
-        {text: "Delete", onclick: "deleteEpisodeAction();", type: "primary"}
-    ]);
-}
-
-function deleteEpisodeAction() {
-    $(".dialog button:last").attr("disabled", "true");
-
-    firebase.database().ref("orgs/" + currentUser.orgName + "/programmes/" + getURLParameter("prog") + "/episodes/" + useEpisodeKey).remove().then(function() {
-        $(".dialog button:last").attr("disabled", null);
-
-        closeDialog();
-    }).catch(function() {
-        $(".dialog button:last").attr("disabled", null);
-
-        $(".dialogError").text("Your episode could not be deleted. Please try again later.");
     });
 }
 
@@ -189,5 +191,159 @@ function editProgrammeDescriptionAction() {
         $(".dialog button:last").attr("disabled", null);
 
         $(".dialogError").text("Your new programme description could not be saved. Please try again later.");
+    });
+}
+
+function newEpisode() {
+    dialog("New episode", `
+        <div>
+            Create a new episode as part of the programme. Templates will be
+            copied over so that you can easily create the episode.
+        </div>
+        <div class="spacedTop">
+            <label>
+                <span>Slug</span>
+                <input placeholder="Enter the name of the episode" class="newEpisodeSlug">
+            </label>
+            <label>
+                <span>First TX date</span>
+                <input type="datetime-local" class="newEpisodeFirstTXDate">
+            </label>
+        </div>
+        <p class="dialogError"></p>
+    `, [
+        {text: "Cancel", onclick: "closeDialog();", type: "secondary"},
+        {text: "Create", onclick: "newEpisodeAction();", type: "primary"}
+    ]);
+}
+
+function newEpisodeAction() {
+    $(".dialog button:last").attr("disabled", "true");
+
+    if ($(".newEpisodeFirstTXDate").val().trim() == "" || $(".newEpisodeFirstTXDate").val().trim() == "" || isNaN(new Date($(".newEpisodeFirstTXDate").val().trim()).getTime())) {
+        $(".dialog button:last").attr("disabled", null);
+
+        $(".dialogError").text("It appears that you have left some fields blank or incorrectly formatted. Please enter data into those fields and try again.");
+    } else {
+        firebase.database().ref("orgs/" + currentUser.orgName + "/programmes/" + getURLParameter("prog") + "/episodes").push().set({
+            slug: $(".newEpisodeSlug").val().trim(),
+            firstTXDate: new Date($(".newEpisodeFirstTXDate").val().trim()).getTime()
+        }).then(function() {
+            $(".dialog button:last").attr("disabled", null);
+
+            closeDialog();
+        }).catch(function() {
+            $(".dialog button:last").attr("disabled", null);
+
+            $(".dialogError").text("Your new episode could not be created. Please try again later.");
+        });
+    }
+}
+
+function deleteEpisode(episodeKey) {
+    useEpisodeKey = episodeKey;
+
+    dialog("Delete episode", `
+        <div>
+            Do you really want to delete this episode? Once deleted, the
+            episode cannot be recovered.
+        </div>
+        <p class="dialogError"></p>
+    `, [
+        {text: "Cancel", onclick: "closeDialog();", type: "secondary"},
+        {text: "Delete", onclick: "deleteEpisodeAction();", type: "primary"}
+    ]);
+}
+
+function deleteEpisodeAction() {
+    $(".dialog button:last").attr("disabled", "true");
+
+    firebase.database().ref("orgs/" + currentUser.orgName + "/programmes/" + getURLParameter("prog") + "/episodes/" + useEpisodeKey).remove().then(function() {
+        $(".dialog button:last").attr("disabled", null);
+
+        closeDialog();
+    }).catch(function() {
+        $(".dialog button:last").attr("disabled", null);
+
+        $(".dialogError").text("Your episode could not be deleted. Please try again later.");
+    });
+}
+
+function newTemplate() {
+    dialog("New template", `
+        <div>
+            Create a new template to use in episodes. Templates can be copied
+            to create new episodes and can be reused again and again.
+        </div>
+        <div class="spacedTop">
+            <label>
+                <span>Slug</span>
+                <input placeholder="Enter the name of the template" class="newTemplateSlug">
+            </label>
+            <label>
+                <span>First TX date</span>
+                <input type="datetime-local" class="newTemplateFirstTXDate">
+            </label>
+        </div>
+        <p class="dialogError"></p>
+    `, [
+        {text: "Cancel", onclick: "closeDialog();", type: "secondary"},
+        {text: "Create", onclick: "newTemplateAction();", type: "primary"}
+    ]);
+}
+
+function newTemplateAction() {
+    $(".dialog button:last").attr("disabled", "true");
+
+    if ($(".newTemplateFirstTXDate").val().trim() == "" || $(".newTemplateFirstTXDate").val().trim() == "" || isNaN(new Date($(".newTemplateFirstTXDate").val().trim()).getTime())) {
+        $(".dialog button:last").attr("disabled", null);
+
+        $(".dialogError").text("It appears that you have left some fields blank or incorrectly formatted. Please enter data into those fields and try again.");
+    } else {
+        firebase.database().ref("orgs/" + currentUser.orgName + "/programmes/" + getURLParameter("prog") + "/templates").push().set({
+            slug: $(".newTemplateSlug").val().trim(),
+            firstTXDate: new Date($(".newTemplateFirstTXDate").val().trim()).getTime()
+        }).then(function() {
+            $(".dialog button:last").attr("disabled", null);
+
+            closeDialog();
+        }).catch(function() {
+            $(".dialog button:last").attr("disabled", null);
+
+            $(".dialogError").text("Your new template could not be created. Please try again later.");
+        });
+    }
+}
+
+function deleteTemplate(templateKey) {
+    useTemplateKey = templateKey;
+
+    dialog("Delete template", `
+        <div>
+            Do you really want to delete this template? Once deleted, the
+            template cannot be recovered.
+        </div>
+        <div class="spacedTop">
+            <strong>Note:</strong> Episodes that have been created by this
+            template will not be deleted.
+        </p>
+        <p class="dialogError"></p>
+    `, [
+        {text: "Cancel", onclick: "closeDialog();", type: "secondary"},
+        {text: "Delete", onclick: "deleteTemplateAction();", type: "primary"}
+    ]);
+}
+
+function deleteTemplateAction() {
+    $(".dialog button:last").attr("disabled", "true");
+
+    firebase.database().ref("orgs/" + currentUser.orgName + "/programmes/" + getURLParameter("prog") + "/templates/" + useTemplateKey).remove().then(function() {
+        $(".dialog button:last").attr("disabled", null);
+
+        closeDialog();
+    }).catch(function() {
+        $(".dialog button:last").attr("disabled", null);
+
+        $(".dialogError").text("Your template could not be deleted. Please try again later.");
     });
 }
