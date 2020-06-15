5067376c4da6400b55a075c38c8b4184153491b5
James-Livesey <jamesliveg@gmail.com>
2020-06-15 11:11:50 +0100

    Add time counters to teleprompter

--- progEpisodeDisplay.js
+++ progEpisodeDisplay.js
@@ -1,5 +1,8 @@
 const HUMAN_AVERAGE_WPM = 150;
 
+var durationStart = new Date();
+var durationEnd = new Date();
+
 function getTextDuration(text) {
     return text.trim().split(" ").length * (60 / HUMAN_AVERAGE_WPM) * 1000;
 }
@@ -32,6 +35,47 @@ function loadTeleprompterScript() {
 events.userReady.push(function() {
     loadTeleprompterScript();
 
+    setInterval(function() {
+        var currentTime = new Date();
+
+        $(".currentTime").text(
+            String(currentTime.getHours()).padStart(2, "0") + ":" +
+            String(currentTime.getMinutes()).padStart(2,"0") + ":" +
+            String(currentTime.getSeconds()).padStart(2, "0") + ";" +
+            String(Math.floor(currentTime.getMilliseconds() * 24 / 1000)).padStart(2, "0")
+        );
+
+        var elapsedDuration = currentTime.getTime() - durationStart.getTime();
+        var elapsedHours = Math.floor((elapsedDuration / 1000) / 3600);
+        var elapsedMinutes = Math.floor((Math.floor(elapsedDuration / 1000) % 3600) / 60);
+        var elapsedSeconds = (Math.floor(elapsedDuration / 1000) % 3600) % 60;
+        var elapsedFrames = Math.floor((elapsedDuration % 1000) * 24 / 1000);
+
+        $(".elapsedTime").text(
+            String(elapsedHours).padStart(2, "0") + ":" +
+            String(elapsedMinutes).padStart(2,"0") + ":" +
+            String(elapsedSeconds).padStart(2, "0") + ";" +
+            String(elapsedFrames).padStart(2, "0")
+        );
+
+        var remainingDuration = durationEnd.getTime() - currentTime.getTime();
+        var remainingHours = Math.floor((remainingDuration / 1000) / 3600);
+        var remainingMinutes = Math.floor((Math.floor(remainingDuration / 1000) % 3600) / 60);
+        var remainingSeconds = (Math.floor(remainingDuration / 1000) % 3600) % 60;
+        var remainingFrames = Math.floor((remainingDuration % 1000) * 24 / 1000);
+
+        if (remainingDuration >= 0) {
+            $(".remainingTime").text(
+                String(remainingHours).padStart(2, "0") + ":" +
+                String(remainingMinutes).padStart(2,"0") + ":" +
+                String(remainingSeconds).padStart(2, "0") + ";" +
+                String(remainingFrames).padStart(2, "0")
+            );
+        } else {
+            $(".remainingTime").text("END");
+        }
+    });
+
     firebase.database().ref(episodePath + "/sessions/" + getURLParameter("session")).on("value", function(snapshot) {
         if (snapshot.val().command == "scrollSection") {
             var from = snapshot.val().from || 0;
@@ -39,6 +83,9 @@ events.userReady.push(function() {
 
             function scrollElement() {
                 $("html").scrollTop(currentScrollElement.offset().top - 20 - ($(window).height() / 2));
+
+                durationStart = new Date();
+                durationEnd = new Date(new Date().getTime() + (Number(currentScrollElement.attr("data-duration") * 1000) || getTextDuration(currentScrollElement.text())));
 
                 $("html").animate({
                     scrollTop: currentScrollElement.offset().top + currentScrollElement.height() - ($(window).height() / 2),
